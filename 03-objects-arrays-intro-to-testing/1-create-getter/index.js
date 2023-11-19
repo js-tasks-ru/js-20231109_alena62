6e3165c4ec7f590e2a5b5c1deba1e3874363ff7c
Alena Karpova <avkarpova@avito.ru>
2023-11-19 21:13:10 +0300

    реализована createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,21 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+export function createGetter(path) 
+{
+    const pathArr = path.split('.');
 
-}
+    return function(obj)
+    {
+      let value = obj
+      for (const node of pathArr)
+        {
+        value = value[node]
+        if (value == undefined)
+            {
+                return;
+            }
+        }
+      return value    
+    }
+};
