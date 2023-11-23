4a535af79c70ed9d1da700bb19eed67cf2da0793
Alena Karpova <avkarpova@avito.ru>
2023-11-23 10:15:44 +0300

    add trim function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let lastValue = ''
+    let counter = 0;
+    let result = ''
 
-}
+    if (size === undefined) {
+        return string;
+    }
+
+
+    for (const i of string){
+
+        if (i === lastValue){
+            counter ++;
+        } else {
+            counter = 1;
+        }
+    
+        if (counter <= size) {
+            result += i;
+          }
+       
+          lastValue = i;
+        }
+       
+    return result;
+      }
+
+
+
