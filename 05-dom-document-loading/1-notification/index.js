a377855ebe995ce6de089c4ee954435d766fa94a
Alena Karpova <avkarpova@avito.ru>
2023-11-29 21:12:11 +0300

    document-loading

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
+    constructor(message='', {duration=0,type=""}={}) {
+        this.duration = duration;
+        this.type = type;
+        this.message = message;
+        this.element = this.createElement(this.createTemplate());
+        this.timeoutId;  
+
+        
+    }
+
+    createElement(template){
+        const element = document.createElement('div');
+        element.innerHTML = template;
+        return element.firstElementChild;
+    }
+
+    show(targetTag = document.body){
+        targetTag.append(this.element)
+        this.timeoutId= setTimeout(this.remove, this.duration);
+
+    }
+
+    createTemplate(){
+        return `<div class="notification ${this.type}" style="--value: ${this.duration}ms">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body"> ${this.message}</div>
+                </div>
+                </div>`;
+    }
+
+    remove = () => {
+        this.element.remove();
+      }
+
+      destroy = () => {
+        this.remove();
+        if (this.timeoutId){
+            clearTimeout(this.timeoutId)
+        }
+      }
 
 }
+
